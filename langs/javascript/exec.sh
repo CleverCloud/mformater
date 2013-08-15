794ba95e24083863100b87c26ab21ffdf853ee10
Quentin ADAM <quentin.adam@waxzce.org>
2013-08-15 18:03:10 +0200

    new jsformat

--- langs/javascript/exec.sh
+++ langs/javascript/exec.sh
@@ -1,7 +1,7 @@
 for FILE in `find . -name "*.js" ! -name "*.min.js" ! -name "*.noformat.js" -type f` 
 do
 	echo "js format $FILE"
-	js-beautify -j -f --brace-style=end-expand --outfile=$FILE.new $FILE 
+	js-beautify -s 4 -j --brace-style=end-expand --outfile $FILE.new -f $FILE 
 done
 
 for FILE in `find . -name "*.js.new" -type f` 
